fdcbce74acd14f37e35754445cbc6600ebb9d9e6
kylesallette <kylesallette@gmail.com>
2018-02-07 15:17:40 -0700

    sorry i really need to work on tdd :(

--- spec/features/user_can_see_name_of_student_spec.rb
+++ spec/features/user_can_see_name_of_student_spec.rb
@@ -1,12 +1,34 @@
 require "rails_helper"
 
   describe "as a user" do
-    describe "when i visit /students/:id"  do
-      it "they see the name of the student"  do
-        @student = Student.create!(name: "Kyle")
-        visit student_path
-
-        expect(page).to have_content("Kyle")
-      end
+  describe "when i visit /students/:id"  do
+    it "they see the name of the student"  do
+      @student = Student.create!(name: "Kyle")
+      visit students_path
+      expect(page).to have_content("Kyle")
     end
-  end 
+  end
+  describe "as a user" do
+  describe "when i visit /students/:id"  do
+    it "they see the name of the multiple students"  do
+      @student = Student.create!(name: "Kyle")
+      @student = Student.create!(name: "Mary")
+      visit students_path
+        expect(page).to have_content("Kyle")
+         expect(page).to have_content("Mary")
+         end
+        end
+     end
+
+     describe "as a user" do
+     describe "when they click Create new student" do
+       it "They are taking to the new page" do
+
+       visit students_path
+       click_link('Create new student')
+
+       expect(page).to have_content("Name")
+     end
+   end
+  end
+end
