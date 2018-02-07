15c4e5a9c4a490a71c4359125210ec7b63f9673f
kylesallette <kylesallette@gmail.com>
2018-02-07 13:34:34 -0700

    tdd yeah you know it !!

--- spec/features/user_can_see_name_of_student_spec.rb
+++ spec/features/user_can_see_name_of_student_spec.rb
@@ -3,4 +3,10 @@ require "rails_helper"
   describe "as a user" do
     describe "when i visit /students/:id"  do
       it "they see the name of the student"  do
+        @student = Student.create!(name: "Kyle")
         visit student_path
+
+        expect(page).to have_content("Kyle")
+      end
+    end
+  end 
