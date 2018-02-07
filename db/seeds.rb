5ea7cd9e81cd3bba526251d59ea943b436c4f825
kylesallette <kylesallette@gmail.com>
2018-02-07 13:24:06 -0700

    adding seeds for students

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+STUDENTS = ["kyle", "bob", "sal", "mike", "zack", "ingago", "mary", "anan"]
+
+
+STUDENTS.each do |name|
+  student = Student.create!(name: name)
+  puts "Created #{student.name}"
+end
