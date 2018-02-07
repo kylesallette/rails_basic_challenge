4dde9ca942bbdc7d254563c907a685725e5d6437
kylesallette <kylesallette@gmail.com>
2018-02-07 14:09:23 -0700

    adding new controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,32 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def index
+    @students = Student.all
+  end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "#{@student.name} added!"
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
+  end
 end
